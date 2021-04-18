e63035101b8ada40ab495994793c8b2befbb7a35
Mihail Mihaylov <chakala1975@gmail.com>
2021-04-18 18:36:42 +0300

    fix tests

--- tests/integration/test_item.py
+++ tests/integration/test_item.py
@@ -4,20 +4,19 @@ import unittest
 
 from tests.test_base import TestBase
 
-# from models.item import ItemModel
+from models.item import ItemModel
 
 
 class ItemTest(TestBase):
     def test_crud(self):
         with self.app.app_context():
-            pass
-            # item = ItemModel("test", 19.99)
+            item = ItemModel("test", 19.99, 1)
             # Check does not exist in database
-            # self.assertIsNone(ItemModel.find_by_name("test"))
+            self.assertIsNone(ItemModel.find_by_name("test"))
             # Add to database
-            # item.save_to_db()
+            item.save_to_db()
             # Check exist in database
-            # self.assertIsNotNone(ItemModel.find_by_name("test"))
+            self.assertIsNotNone(ItemModel.find_by_name("test"))
 
 
 if __name__ == "__main__":
