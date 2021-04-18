1fb173d009695d1af48e2215fbddca9f6f98b772
Mihail Mihaylov <chakala1975@gmail.com>
2021-04-18 18:33:03 +0300

    Merge with starter2

--- tests/unit/test_item.py
+++ tests/unit/test_item.py
@@ -1,10 +1,23 @@
 import unittest
+from models.item import ItemModel
+from tests.test_base import TestBase
 
-# from models.item import ItemModel
 
+class TestItem(TestBase):
+    def test_crud(self):
+        with self.app.app_context():
+            item = ItemModel("test", 19.99, 1)
 
-class TestItem(unittest.TestCase):
-    pass
+            self.assertIsNone(
+                ItemModel.find_by_name("test"),
+                "Found an item with name {}, but expected not to.".format(item.name),
+            )
+
+            item.save_to_db()
+
+            self.assertIsNotNone(ItemModel.find_by_name("test"))
+
+            item.delete_from_db()
 
 
 if __name__ == "__main__":
