b49ce4745c24251ba29db169d343d8980d66e326
Mihail Mihaylov <chakala1975@gmail.com>
2021-04-18 18:20:10 +0300

    Merge with starter2

--- models/item.py
+++ models/item.py
@@ -1,29 +1,43 @@
+"""Item module."""
 from db import db
 
 
 class ItemModel(db.Model):
-    __tablename__ = 'items'
+    """ItemModel class."""
+
+    __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
-    def __init__(self, name, price):
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"))
+    store = db.relationship("StoreModel")
+
+    def __init__(self, name, price, store_id):
         self.name = name
         self.price = price
+        self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
+        """Convert to JSON."""
+        return {"name": self.name, "price": self.price}
 
     @classmethod
     def find_by_name(cls, name):
+        """Find by name."""
         return cls.query.filter_by(name=name).first()
 
     def save_to_db(self):
         """Save to db."""
+        # pylint: disable=E1101
         db.session.add(self)
+        # pylint: disable=E1101
         db.session.commit()
 
     def delete_from_db(self):
+        """Delete from db."""
+        # pylint: disable=E1101
         db.session.delete(self)
+        # pylint: disable=E1101
         db.session.commit()
